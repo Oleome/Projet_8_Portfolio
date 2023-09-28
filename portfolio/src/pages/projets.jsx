b94875550df2918d9eff19b97ed30fe09aae2120
Bilou <pierrepillot0110@gmail.com>
2023-09-28 22:11:38 +0200

    up

--- portfolio/src/pages/projets.jsx
+++ portfolio/src/pages/projets.jsx
@@ -6,49 +6,49 @@ import jsonData from '../data/data.json';
 import Card from '../components/Card/card';
 
 function Projets() {
-  const [projectShown, setProjectShown] = useState(null);
-  const [showModal, setShowModal] = useState(false);
+    const [projectShown, setProjectShown] = useState(null);
+    const [showModal, setShowModal] = useState(false);
 
-  const openModal = (project) => {
-    setProjectShown(project);
-    setShowModal(true);
-  };
+    const openModal = (project) => {
+        setProjectShown(project);
+        setShowModal(true);
+    };
 
-  const closeModal = () => {
-    setProjectShown(null);
-    setShowModal(false);
-  };
+    const closeModal = () => {
+        setProjectShown(null);
+        setShowModal(false);
+    };
 
   return (
     <main className="main--projet">
-      <div className="title--container">
-        <h2 className="projets">Mes projets</h2>
-        <img src={ordi} alt="une icône d'ordinateur" />
-      </div>
-      <div className="container" onClick={openModal}>
-        {jsonData.map((item) => (
-          <Card
-            key={item.id}
-            id={item.id}
-            cover={item.cover}
-            pastille={item.pastille}
-            title={item.title}
-            description={item.description}
-            onClick={() => openModal(item)}
-          />
-        ))}
-      </div>
+        <div className="title--container">
+            <h2 className="projets">Mes projets</h2>
+            <img src={ordi} alt="une icône d'ordinateur" />
+        </div>
+        <div className="container">
+            {jsonData.map((item) => (
+                <Card
+                    key={item.id}
+                    id={item.id}
+                    cover={item.cover}
+                    pastille={item.pastille}
+                    title={item.title}
+                    description={item.description}
+                    onClick={() => openModal(item)}
+                />
+            ))}
+        </div>
 
-      {showModal && (
-        <Modal
-          key={projectShown.id}
-          id={projectShown.id}
-          showModal={showModal}
-          setShowModal={closeModal}
-          pics={projectShown.pics}
-          texts={projectShown.texts}
-        />
-      )}
+            {showModal && (
+                <Modal
+                    key={projectShown.id}
+                    id={projectShown.id}
+                    showModal={showModal}
+                    setShowModal={closeModal}
+                    pics={projectShown.pics}
+                    texts={projectShown.texts}
+                />
+            )}
     </main>
   );
 }
